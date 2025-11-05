15a664ae7cd7db719436d0e59718ed3da293ff4b
Rick Hanlon <rickhanlonii@meta.com>
2025-11-05 12:35:04 -0500

    profiling build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,15 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // @ts-ignore
+      "react-dom/client": path.resolve(
+        __dirname,
+        "node_modules/react-dom/profiling",
+      ),
+    },
+  },
+});
